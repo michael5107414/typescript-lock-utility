bcc1824fafa6e84e43688434f08e7d883a7d46c3
Michael Chien <michael5107414@gmail.com>
2024-07-11 00:29:06 +0800

    fix: adjust lockGuard test

--- __tests__/lockGuard.test.ts
+++ __tests__/lockGuard.test.ts
@@ -3,96 +3,74 @@ import { sleepFor } from "./support/util";
 
 describe("LockGuard with Mutex", () => {
   let mutex: Mutex;
-  let flag = false;
-  let missCnt = 0;
-  let executionCnt = 0;
+  let value = 0;
 
-  async function asyncFunc(): Promise<void> {
+  async function asyncFunc(): Promise<number> {
     using _ = await LockGuard.create(mutex);
-    if (flag) {
-      missCnt++;
-    }
-    flag = true;
+    value++;
     await sleepFor(100);
-    if (!flag) {
-      missCnt++;
-    }
-    flag = false;
-    executionCnt++;
+    return value;
   }
 
   beforeEach(() => {
     mutex = new Mutex();
-    missCnt = 0;
-    executionCnt = 0;
+    value = 0;
   });
 
   test("functions do not interfere with each other", async () => {
     const length = 5;
-    await Promise.allSettled(Array.from({ length }, asyncFunc));
-    expect(executionCnt).toBe(length);
-    expect(missCnt).toBe(0);
+    const results = await Promise.all(Array.from({ length }, asyncFunc));
+    expect(results.sort()).toEqual(Array.from({ length }, (_, k) => k + 1));
   });
 
   test("functions complete in the correct order", async () => {
     const length = 3;
-    Promise.allSettled(Array.from({ length }, asyncFunc));
-    expect(executionCnt).toBe(0);
+    const results = await Promise.all(Array.from({ length }, asyncFunc));
+    expect(results.sort()).toEqual(Array.from({ length }, (_, k) => k + 1));
 
-    await asyncFunc();
-    const promiseObj = asyncFunc();
-    expect(executionCnt).toBe(length + 1);
-
-    await promiseObj;
-    expect(executionCnt).toBe(length + 2);
-    expect(missCnt).toBe(0);
+    const resultPromise = asyncFunc();
+    await sleepFor(10);
+    const result2 = await asyncFunc();
+    await sleepFor(10);
+    const result1 = await resultPromise;
+    expect(result1).toBe(length + 1);
+    expect(result2).toBe(length + 2);
   });
 });
 
 describe("LockGuard with SharedMutex", () => {
-  let sharedMutex: SharedMutex;
-  let flag = false;
-  let missCnt = 0;
-  let executionCnt = 0;
+  let mutex: SharedMutex;
+  let value = 0;
 
-  async function asyncFunc(): Promise<void> {
-    using _ = await LockGuard.create(sharedMutex);
-    if (flag) {
-      missCnt++;
-    }
-    flag = true;
+  async function asyncFunc(): Promise<number> {
+    using _ = await LockGuard.create(mutex);
+    value++;
     await sleepFor(100);
-    if (!flag) {
-      missCnt++;
-    }
-    flag = false;
-    executionCnt++;
+    return value;
   }
 
   beforeEach(() => {
-    sharedMutex = new SharedMutex();
-    missCnt = 0;
-    executionCnt = 0;
+    mutex = new SharedMutex();
+    value = 0;
   });
 
   test("functions do not interfere with each other", async () => {
     const length = 5;
-    await Promise.allSettled(Array.from({ length }, asyncFunc));
-    expect(executionCnt).toBe(length);
-    expect(missCnt).toBe(0);
+    const results = await Promise.all(Array.from({ length }, asyncFunc));
+    expect(results.sort()).toEqual(Array.from({ length }, (_, k) => k + 1));
   });
 
   test("functions complete in the correct order", async () => {
     const length = 3;
-    Promise.allSettled(Array.from({ length }, asyncFunc));
-    expect(executionCnt).toBe(0);
+    const results = await Promise.all(Array.from({ length }, asyncFunc));
+    expect(results.sort()).toEqual(Array.from({ length }, (_, k) => k + 1));
 
-    await asyncFunc();
-    const promiseObj = asyncFunc();
-    expect(executionCnt).toBe(length + 1);
-
-    await promiseObj;
-    expect(executionCnt).toBe(length + 2);
-    expect(missCnt).toBe(0);
+    const resultPromise = asyncFunc();
+    await sleepFor(10);
+    const result2 = await asyncFunc();
+    await sleepFor(10);
+    const result1 = await resultPromise;
+    expect(result1).toBe(length + 1);
+    expect(result2).toBe(length + 2);
   });
 });
