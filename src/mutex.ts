f477dbfcce0ce651d80bcb657a338035710e2f37
Michael Chien <michaelchien@synology.com>
2024-07-11 16:25:28 +0800

    fix: eliminate race condition

--- src/mutex.ts
+++ src/mutex.ts
@@ -1,21 +1,33 @@
 export interface MutexInterface {
   lock(): Promise<void>;
+  tryLock(): boolean;
   unlock(): void;
 }
 
 export class Mutex implements MutexInterface {
-  private acquired = false;
-  private queue = [] as Array<() => void>;
+  private _acquired = false;
+  private _queue = [] as Array<() => void>;
 
   async lock(): Promise<void> {
-    if (this.acquired) {
-      await new Promise<void>((resolve) => this.queue.push(resolve));
+    if (this._acquired) {
+      await new Promise<void>((resolve) => this._queue.push(resolve));
     }
-    this.acquired = true;
+    this._acquired = true;
+  }
+
+  tryLock(): boolean {
+    if (this._acquired) {
+      return false;
+    }
+    this._acquired = true;
+    return true;
   }
 
   unlock(): void {
-    this.acquired = false;
-    this.queue.shift()?.();
+    if (this._queue.length === 0) {
+      this._acquired = false;
+    } else {
+      this._queue.shift()();
+    }
   }
 }
--- src/sharedMutex.ts
+++ src/sharedMutex.ts
@@ -2,49 +2,83 @@ import { MutexInterface } from "./mutex";
 
 export interface SharedMutexInterface extends MutexInterface {
   lockShared(): Promise<void>;
+  tryLockShared(): boolean;
   unlockShared(): void;
 }
 
-export class SharedMutex implements SharedMutex {
-  private acquiredCnt = 0;
-  private isShared = true;
-  private queue = [] as Array<{ shared: boolean; resolve: () => void }>;
+export class SharedMutex implements SharedMutexInterface {
+  private _acquired = false;
+  private _acquiredCnt = 0;
+  private _isShared = true;
+  private _queue = [] as Array<{ shared: boolean; resolve: () => void }>;
 
   async lock(): Promise<void> {
-    if (this.acquiredCnt > 0) {
+    if (this._acquired) {
       await new Promise<void>((resolve) =>
-        this.queue.push({ shared: false, resolve }),
+        this._queue.push({ shared: false, resolve }),
       );
     }
-    this.acquiredCnt++;
-    this.isShared = false;
+    this._acquired = true;
+    this._acquiredCnt++;
+    this._isShared = false;
   }
 
-  async lockShared(): Promise<void> {
-    if (!this.isShared || this.queue.length > 0) {
-      await new Promise<void>((resolve) =>
-        this.queue.push({ shared: true, resolve }),
-      );
+  tryLock(): boolean {
+    if (this._acquired) {
+      return false;
     }
-    this.acquiredCnt++;
+    this._acquired = true;
+    this._acquiredCnt++;
+    this._isShared = false;
+    return true;
   }
 
   unlock(): void {
-    this.acquiredCnt--;
-    this.isShared = true;
-    while (this.queue.length > 0) {
-      if (!this.queue[0].shared) {
-        this.queue.shift()?.resolve();
-        break;
+    this._acquiredCnt--;
+    console.log(this._queue);
+    if (this._queue.length === 0) {
+      this._isShared = true;
+      this._acquired = false;
+    } else if (!this._queue[0].shared) {
+      this._isShared = false;
+      this._queue.shift().resolve();
+    } else {
+      this._isShared = true;
+      console.log(this._queue);
+      while (this._queue.length > 0 && this._queue[0].shared) {
+        this._queue.shift().resolve();
       }
-      this.queue.shift()?.resolve();
     }
   }
 
+  async lockShared(): Promise<void> {
+    if ((this._acquired && !this._isShared) || this._queue.length > 0) {
+      await new Promise<void>((resolve) =>
+        this._queue.push({ shared: true, resolve }),
+      );
+    }
+    this._acquired = true;
+    this._acquiredCnt++;
+  }
+
+  tryLockShared(): boolean {
+    if ((this._acquired && !this._isShared) || this._queue.length > 0) {
+      return false;
+    }
+    this._acquired = true;
+    this._acquiredCnt++;
+    return true;
+  }
+
   unlockShared(): void {
-    this.acquiredCnt--;
-    if (this.acquiredCnt === 0) {
-      this.queue.shift()?.resolve();
+    this._acquiredCnt--;
+    if (this._acquiredCnt !== 0) {
+      return;
+    } else if (this._queue.length === 0) {
+      this._acquired = false;
+    } else {
+      this._isShared = false;
+      this._queue.shift().resolve();
     }
   }
 }
