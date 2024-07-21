651341b4991d8d675241ed24378a5dc7d7120a26
Michael Chien <michael5107414@gmail.com>
2024-07-21 16:08:25 +0800

    chore: add comment for semaphore and condition variable

--- src/conditionVariable.ts
+++ src/conditionVariable.ts
@@ -3,15 +3,27 @@ import { BasicLockableInteface } from "./types";
 export class ConditionVariable {
   private _queue: Array<() => void> = [];
 
+  /**
+   * Triggers the execution of the first waiting function in the queue.
+   */
   notifyOne(): void {
     this._queue.shift()?.();
   }
 
+  /**
+   * Triggers the execution of all waiting functions in the queue.
+   */
   notifyAll(): void {
     this._queue.forEach((resolve) => resolve());
     this._queue = [];
   }
 
+  /**
+   * Waits for the condition variable to be notified.
+   * @param lock The lock which must be locked by the caller
+   * @param predicate The predicate to check whether the waiting can be completed
+   * @throws If the lock does not own the lock.
+   */
   async wait(lock: BasicLockableInteface, predicate?: () => boolean | Promise<boolean>): Promise<void> {
     if (!lock.ownsLock()) {
       throw new Error("lock should own the lock by calling wait");
--- src/semaphore.ts
+++ src/semaphore.ts
@@ -1,8 +1,14 @@
 export class Semaphore {
   private _queue: Array<() => void> = [];
 
+  /**
+   * @param _desired The number of permits that are available to be acquired.
+   */
   constructor(private _desired: number) {}
 
+  /**
+   * Acquires a permit from this semaphore, blocking until one is available.
+   */
   async acquire(): Promise<void> {
     if (this._desired > 0) {
       this._desired--;
@@ -13,6 +19,10 @@ export class Semaphore {
     }
   }
 
+  /**
+   * Try to acquires a permit from this semaphore
+   * @returns true if a permit was acquired, false otherwise.
+   */
   tryAcquire(): boolean {
     if (this._desired > 0) {
       this._desired--;
@@ -21,6 +31,11 @@ export class Semaphore {
     return false;
   }
 
+  /**
+   * Releases a permit, returning it to the semaphore.
+   * @param update The number of permits to release.
+   * @throws If the update is negative or not an integer.
+   */
   release(update = 1): void {
     if (update < 0 || !Number.isInteger(update)) {
       throw new Error("update must be a non-negative integer");
